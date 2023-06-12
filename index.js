6304db9be5022e3c77003961f25a7f7cae968f9d
Alan Luna <85804890+AlanLuna-00@users.noreply.github.com>
2023-06-12 18:56:07 -0300

    alan messi

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function laCajaDePandora(numero) {
-    if (numero % 2 === 0) {
-        return numero.toString(2); // Convierte el número par a binario y lo retorna
-    } else {
-        return numero.toString(16); // Convierte el número impar a hexadecimal y lo retorna
-    }
+  if (numero % 2 === 0) {
+    return numero.toString(2); // Convierte el número par a binario y lo retorna
+  } else {
+    return numero.toString(16); // Convierte el número impar a hexadecimal y lo retorna
+  }
 }
 
 function obtenerInformacionPersonal() {
-    return {
-        nombre: "Nombre",
-        edad: 25,
-        nacionalidad: "Nacionalidad"
-    };
+  return {
+    nombre: "Nombre",
+    edad: 25,
+    nacionalidad: "Nacionalidad",
+  };
 }
 
 // Ejemplo de uso
@@ -19,3 +19,6 @@ console.log(laCajaDePandora(10)); // Output: '1010' (10 en binario)
 console.log(laCajaDePandora(7)); // Output: '7' (7 en hexadecimal)
 console.log(obtenerInformacionPersonal());
 
+const MESSI_MESSI_MESSI = (messi) => {
+  return messi;
+};
